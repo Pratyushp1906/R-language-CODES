8bef933f8589ce36bffa2c20ffaa694c8bc32cb4
Pratyushp1906 <pratyushp1906@gmail.com>
2023-01-25 22:43:39 +0530

    Operations using two variables

---
+++ da_lab_2_q-3.R
@@ -0,0 +1,10 @@
+a = as.integer(readline("Enter the value of a: "))
+b = as.integer(readline("Enter the value of b: "))
+sum=a+b
+cat("sum=",sum)
+diff=a-b
+cat("Difference",diff)
+mul=a*b
+cat("Multiplication",mul)
+div=a/b
+cat("Division",div)
