d2619ad8ff72be7046275d31e5682d990148ee13
Pratyushp1906 <pratyushp1906@gmail.com>
2023-01-31 17:49:11 +0530

    To print the maximum and minimum element among three numbers

---
+++ da_lab_3_q_3.R
@@ -0,0 +1,12 @@
+a = as.integer(readline("Enter the no."))
+b = as.integer(readline("Enter the no."))
+c = as.integer(readline("Enter the no."))
+if(a>b & a>c){
+  cat(a)
+}else if(b>a & b>c){
+  cat(b)
+}else{
+  cat(c)
+}̥
+
+ 
